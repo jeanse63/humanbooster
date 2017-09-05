603d4255393dd4aab3ae004f0c112b8971059ac4
Gilou63 <gbourquard63@gmail.com>
2017-09-05 14:56:11 +0200

    Modif task

--- src/main/java/com/humanboster/todolist/Task.java
+++ src/main/java/com/humanboster/todolist/Task.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+/* Changement de Gilles */
 public class Task{
+    // Proprietes	
     private String title;
     private String dateDebut;
     private String dateFin;
     private List<Task> sousTache = new ArrayList<Task>();
 
+    // Constructeur
     public Task(String title, String dateDebut, String dateFin) {
         this.title = title;
         this.dateDebut = dateDebut;
