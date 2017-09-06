72d5b93eefbfa7c22bc4c763f1b566ff377a015e
Gilou63 <gbourquard63@gmail.com>
2017-09-06 11:19:09 +0200

    modif 2 Gilles

--- src/main/java/com/humanbooster/todolist/TaskModel.java
+++ src/main/java/com/humanbooster/todolist/TaskModel.java
@@ -106,7 +106,7 @@ public class TaskModel {
            
             if (idFath != -1) {
                 Task tFather = getTask(idFath);
-                infoFather += "<br> Parent task : " + tFather.getName();
+                infoFather += " Parent task : " + tFather.getName();
             }
             return "<div>Tache -> <a href='/ToDoList/showTask/" + t.getId() + "'> " + t.getName() + " </a> : " + t.getDesc() + infoFather + "<br>  </div>";
 
