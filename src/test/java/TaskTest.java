bbe7f84ce0d94b1b4800d1e9fa3d0914923accf5
jeanse63 <jeanse63@gmail.com>
2017-09-07 09:37:31 +0200

    js

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import com.humanbooster.todolist.Task;
+import com.humanbooster.todolist.TaskModel;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TaskTest {
+
+    @Test
+    public void DeleteTask(){
+
+        Task task = new Task(-1,1,06/09/2017, 10/09/2017, "tache 1", "tache 1");
+        assertEquals();
+    }
 }
