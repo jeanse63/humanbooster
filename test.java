c057ed683c9d4604b0d7dee8182db1a11998a22c
unknown <dimitripireyre@live.fr>
2017-09-07 15:56:49 +0200

    test slack

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-aeeeeaaa
-=======
-jbdvdhvqfhbveqhfvbrezfhezucbuzebfaaaa
->>>>>>> cb456ab5e07020ad3704b93c1e54b604ed418fd1
+<<<<<<< HEAzkfvonjkdsndksnbjdsnbjnZBVD
+aeeeeaaaBEQBSDB
+=======B
+jbdvdhvqBfhbveqhfvbrezfhezucbuzebfaaaa
+>>>>>>> cFDBFDBDDFBBQDQDFBFDBBbBD4B5QDB6QDBFDBQDBQBab5e07020ad3704b93c1e54b604ed418fd1
